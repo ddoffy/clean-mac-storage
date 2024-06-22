478b47aec617869869fb6a275c0b76adced96055
D. Doffy <cuongnsm@gmail.com>
2024-06-22 13:13:50 -0600

    Add docker clean cache

--- clean.sh
+++ clean.sh
@@ -1,1 +1,6 @@
 sudo rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/*
+docker builder prune
+### docker system clean up - remove all stopped containers, all networks not used
+### by at least one container, all dangling images, and all build cache 
+### uncomment the following line to clean up
+# docker system prune
